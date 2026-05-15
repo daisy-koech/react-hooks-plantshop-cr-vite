ca9e29dd1e87690a97d547b7a273badbde407962
Daisy Koech <koechdaisy86@gmail.com>
2026-05-15 11:00:24 +0300

    initial commit

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,4 +1,3 @@
 import './test_suites/AllPlants.test'
 import './test_suites/CreatePlant.test'
-import './test_suites/InStock.test'
 import './test_suites/SearchPlants.test'
--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants]=useState([])
+  const [search, setSearch] = useState("");
+  
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then((response) => {
+      if(!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`)
+      }
+      return response.json();
+    })
+    .then(data => setPlants(data))
+    .catch(error => console.error("Error fetching data:", error))
+
+  }, [])
+
+  function handleAddPlant(newPlant) {
+    setPlants([...plants, newPlant]);
+  }
+
+  const filteredPlants = plants.filter((plant) => plant.name.toLowerCase().includes(search.toLowerCase()))
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search search={search} setSearch={setSearch}/>
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
